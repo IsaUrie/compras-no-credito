1779213fe61c28c6ffdbd854882dd4c6fef4f935
isabela.silva <isabela.silva@agi.com.br>
2024-02-13 17:33:58 -0300

    Reestruturacao do projeto
    comecando novamente

--- src/menu/MenuPrincipal.java
+++ src/menu/MenuPrincipal.java
@@ -1,48 +1,5 @@
 package menu;
 
-import modelos.CartaoCredito;
-
-import java.util.Scanner;
-
 public class MenuPrincipal {
-    CartaoCredito meuCartao = new CartaoCredito(0);
 
-    public int opcaoEscolhida = 0;
-
-    public void funcionamentoMenu(){
-        while (opcaoEscolhida < 2){
-        escolherOpcoes(pedirOpcao());
-        }
-    }
-    private int pedirOpcao(){
-        mostrarOpcoes();
-        Scanner scanner = new Scanner(System.in);
-        opcaoEscolhida = scanner.nextInt();
-        return opcaoEscolhida;
-    }
-    private void pedirLimiteCartao(){
-        System.out.println("Informe o limite do seu cartao: ");
-    }
-
-    public void definirLimite(){
-        Scanner scanner = new Scanner(System.in);
-        pedirLimiteCartao();
-        meuCartao.setLimiteCartao(scanner.nextDouble());
-
-    }
-    public void mostrarLimite(){
-        System.out.println(meuCartao.getLimiteCartao());
-    }
-    private void mostrarOpcoes(){
-        System.out.println("Digite 0 para visualizar o limite do seu cartao. ");
-        System.out.println("Digite 1 para definir o limite do seu cartao. ");
-        System.out.println("Digite 2 para sair. ");
-    }
-    public void escolherOpcoes(int opcaoEscolhida){
-        if (opcaoEscolhida == 0) {
-            mostrarLimite();
-        } else if (opcaoEscolhida == 1){
-            definirLimite();
-        }
-    }
 }
--- src/modelos/CartaoCredito.java
+++ src/modelos/CartaoCredito.java
@@ -1,17 +1,5 @@
 package modelos;
 
 public class CartaoCredito {
-    private double limiteCartao;
 
-    public CartaoCredito(double limiteCartao) {
-        this.limiteCartao = limiteCartao;
-    }
-
-    public double getLimiteCartao() {
-        return limiteCartao;
-    }
-
-    public void setLimiteCartao(double limiteCartao) {
-        this.limiteCartao = limiteCartao;
-    }
 }
