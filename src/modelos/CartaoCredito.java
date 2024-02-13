04065f2097ea2eba5289c483c92c29691581cda8
isabela.silva <isabela.silva@agi.com.br>
2024-02-13 17:38:55 -0300

    Definindo entidades
    Define e faz o encapsulamento das classes CartaoCredito e Compras

--- src/modelos/CartaoCredito.java
+++ src/modelos/CartaoCredito.java
@@ -1,5 +1,40 @@
 package modelos;
 
-public class CartaoCredito {
+import java.util.ArrayList;
+import java.util.List;
 
+public class CartaoCredito {
+    private double limite;
+    private double saldo;
+    private List<Compras> compras;
+
+    public CartaoCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.compras = new ArrayList<>();
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public List<Compras> getCompras() {
+        return compras;
+    }
+
+    public void setCompras(List<Compras> compras) {
+        this.compras = compras;
+    }
 }
---
+++ src/modelos/Compras.java
@@ -0,0 +1,27 @@
+package modelos;
+
+public class Compras {
+    private String nomeProduto;
+    private double precoProduto;
+
+    public Compras(String nomeProduto, double precoProduto) {
+        this.nomeProduto = nomeProduto;
+        this.precoProduto = precoProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public double getPrecoProduto() {
+        return precoProduto;
+    }
+
+    public void setPrecoProduto(double precoProduto) {
+        this.precoProduto = precoProduto;
+    }
+}
